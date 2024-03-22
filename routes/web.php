15790652df88b56c0c8cb22a09ccb718bfc6fd83
Gaspar Saldaña Sandoval <gaspar.saldana.sandoval@gmail.com>
2024-03-22 10:10:20 +0100

    job model class added and fixing array issue

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,24 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
+
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'coder',
-                'salary' => '$10.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'designer',
-                'salary' => '$20.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'writer',
-                'salary' => '$15.000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
+
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'coder',
-            'salary' => '$10.000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'designer',
-            'salary' => '$20.000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'writer',
-            'salary' => '$15.000'
-        ]
-        ];
-        $job =Arr::first($jobs, fn($job) =>$job['id'] == $id);
-        //dd($job);
-        return view('job', ['job' => $job]);
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
 });
+
 Route::get('/contact', function () {
     return view('contact');
-});
-Route::get('/team', function () {
-    return view('team');
 });
