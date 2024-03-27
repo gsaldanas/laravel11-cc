b40365b915dc6353f1858f8a9b12bef472e05aa0
Gaspar Saldaña Sandoval <gaspar.saldana.sandoval@gmail.com>
2024-03-27 17:19:46 +0100

    add Job and employer factory with a relationship

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
+    use HasFactory;
 //associate with the model table
 protected $table = 'job_listings';
 //are allowed for mass assign
